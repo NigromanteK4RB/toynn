7ec81dc7e9097238ee1ce73a9ae6d5ea7c01a222
Kevin Adrian Rodriguez Barranco <kevin4rb200116@gmail.com>
2022-04-09 00:12:32 -0400

    "namespaces.py" file created
    the module "namespaces.py" has been created, this module will be where the
    namespaces, "Errors", "Activations", "Regularizations" will be implemented,
    which will contain the error, activation and regularization functions
    respectively.

---
+++ toynn/namespaces.py
@@ -0,0 +1,23 @@
+
+import math
+
+from collections import namedtuple
+
+__all__ = [
+    'Errors',
+    'Activations',
+    'Regularizations'
+]
+
+__all__ = [
+    'Errors',
+    'Activations',
+    'Regularizations'
+]
+
+class Namespace(object):
+    pass
+
+class DerivableFunction(namedtuple("DerivableFunction","function derivative")):
+    pass
+
