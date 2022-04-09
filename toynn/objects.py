14de95000a9bedb1477112cd8883626ccb8ef39d
Kevin Adrian Rodriguez Barranco <kevin4rb200116@gmail.com>
2022-04-09 00:23:07 -0400

    the objects.Node class represents a cell in the neural network.

--- toynn/objects.py
+++ toynn/objects.py
@@ -13,3 +13,48 @@ __all__: list[str] = [
     'Regularizations',
     'Network',
 ]
+
+class Node(object):
+    id: str
+
+    input_links: list
+    output_links: list
+
+    bias: float
+    total_input: float
+    output: float
+    output_derivative: float
+    input_derivative: float
+    acc_input_derivative: float
+    num_accumulated_derivatives: int
+    activation: DerivableFunction
+
+    def __init__(node: object, id: str, activation: DerivableFunction, init_zero: bool = False) -> None:
+        node.id = id
+        node.activation = activation
+        node.bias: float=random.random() * 2 - 1
+
+        node.input_links: list[Link]=list()
+        node.output_links: list[Link]=list()
+
+        node.total_input: float=float()
+        node.output: float=float()
+        node.output_derivative: float=float()
+        node.input_derivative: float=float()
+        node.acc_input_derivative: float=float()
+        node.num_accumulated_derivatives: int=int()
+
+        if init_zero:
+            node.bias = float()
+
+    def updateOutput(node) -> float:
+        node.total_input = node.bias
+
+        for link in node.input_links:
+            node.total_input += link.weight * link.source.output
+
+        node.output = node.activation.function(node.total_input)
+
+        return node.output
+
+
