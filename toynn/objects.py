ebb53b6ec2ca5d8f1ef6b316c20f198acfc0637c
Kevin Adrian Rodriguez Barranco <kevin4rb200116@gmail.com>
2022-04-09 00:36:45 -0400

    the objects.Network class is a set of Layers whose Nodes are interconnected by Links and through these interact to produce a network output.

--- toynn/objects.py
+++ toynn/objects.py
@@ -6,6 +6,8 @@ from .namespaces import DerivableFunction
 from .namespaces import Errors
 from .namespaces import Regularizations
 
+from typing import Callable
+
 __all__: list[str] = [
     'Activations',
     'DerivableFunction',
@@ -87,4 +89,69 @@ class Layer(list[Node]):
     @property
     def output(layer: list[Node]) -> list[float]:
         return [node.output for node in layer]
+
+class Network(list[Layer]):
+
+    @property
+    def output_layer(network: list[Layer]) -> Layer:
+        return network[-1]
+
+    @property
+    def input_layer(network: list[Layer]) -> Layer:
+        return network[0]
+
+    def forEachNode(network: list[Layer], ignore_inputs: bool, accessor: Callable) -> None:
+        for layer in network[1 if ignore_inputs else 0:]:
+            for node in layer:
+                accessor(node)
+
+    @property
+    def outputs(network: list[Layer]) -> list[float]:
+        return network.output_layer.output
+
+    def __init__(network: list[Layer], shape: list[int], hidden_activation: DerivableFunction,
+                 output_activation: DerivableFunction, regularization: DerivableFunction,
+                 input_ids: list[str], init_zero: bool = False) -> None:
+        layers: int=len(shape)
+        id: int=1
+
+        for layer in range(layers):
+            is_output: bool= layer == (layers - 1)
+            is_input: bool=layer == 0
+
+            currentl: Layer=Layer()
+
+            network.append(currentl)
+
+            nodes: int=shape[layer]
+
+            for node in range(nodes):
+                currentn_id: int=str(id)
+                if (is_input):
+                    currentn_id = input_ids[node]
+                else:
+                    id += 1
+
+                currentn: Node=Node(currentn_id, output_activation if is_output else hidden_activation, init_zero)
+
+                currentl.append(currentn)
+
+                if (layer >= 1):
+                    for prevn in network[layer - 1]:
+                        new: Link=Link(prevn, currentn, regularization, init_zero)
+                        prevn.output_links.append(new)
+                        currentn.input_links.append(new)
+
+    def forwardPropagation(network: list[Layer], inputs: list[float]) -> list[float]:
+        if (len(inputs) != len(network.input_layer)):
+            raise ValueError("The number of inputs must match the number of nodes in the input layer")
+
+        for n, node in enumerate(network.input_layer):
+            node.output = inputs[n]
+
+        for layer in network[1:]:
+            for node in layer:
+                node.updateOutput()
+
+        return network.outputs
 
