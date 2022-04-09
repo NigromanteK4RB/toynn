b84490f6ea633f18bbd0ce32d96fbb487fcd8a5c
Kevin Adrian Rodriguez Barranco <kevin4rb200116@gmail.com>
2022-04-09 00:17:17 -0400

    module namespaces implemented
    all namespaces which represent functions and their respective derivative has been implemented.

--- toynn/namespaces.py
+++ toynn/namespaces.py
@@ -9,15 +9,50 @@ __all__ = [
     'Regularizations'
 ]
 
-__all__ = [
-    'Errors',
-    'Activations',
-    'Regularizations'
-]
-
 class Namespace(object):
     pass
 
 class DerivableFunction(namedtuple("DerivableFunction","function derivative")):
     pass
+
+Errors: Namespace=Namespace()
+
+Errors.Square=DerivableFunction(
+    (lambda output, target: 0.5 * math.pow(output - target, 2)),
+    (lambda output, target: output - target),
+)
+
+Activations: Namespace=Namespace()
+
+Activations.Tanh=DerivableFunction(
+    (lambda input: math.tanh(input)),
+    (lambda input: 1 - (math.tanh(input)**2)),
+)
+
+Activations.Relu=DerivableFunction(
+    (lambda input: max(0, input)),
+    (lambda input: 0 if input <= 0 else 1),
+)
+
+Activations.Sigmoid=DerivableFunction(
+    (lambda input: 1 / (1 + math.exp(-input))),
+    (lambda input: (1 / (1 + math.exp(-input))) * (1 - (1 / (1 + math.exp(-input))))),
+)
+
+Activations.Linear=DerivableFunction(
+    (lambda input: input),
+    (lambda input: 1),
+)
+
+Regularizations: Namespace=Namespace()
+
+Regularizations.L1=DerivableFunction(
+    (lambda weight: abs(weight)),
+    (lambda weight: -1 if weight < 0 else 1 if weight > 1 else 0),
+)
+
+Regularizations.L2=DerivableFunction(
+    (lambda weight: 0.5 * (weight**2)),
+    (lambda weight: weight),
+)
 
