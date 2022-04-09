0c3ec88c56b5b901f09a25d14ff0aae1bda03b88
Kevin Adrian Rodriguez Barranco <kevin4rb200116@gmail.com>
2022-04-09 00:20:22 -0400

    objects.py file created.

---
+++ toynn/objects.py
@@ -0,0 +1,15 @@
+
+import random
+
+from .namespaces import Activations
+from .namespaces import DerivableFunction
+from .namespaces import Errors
+from .namespaces import Regularizations
+
+__all__: list[str] = [
+    'Activations',
+    'DerivableFunction',
+    'Errors',
+    'Regularizations',
+    'Network',
+]
